3bdf7729f7e4a00b508e0dfea2e91b9cfda9db37
ludg1e <77110723+ludg1e@users.noreply.github.com>
2023-03-24 12:15:15 +0100

    Implement last parts of a728e10776edfc745c09cd823e8651a197b9caa4 commit

--- components/header.js
+++ components/header.js
@@ -6,7 +6,10 @@ class Header extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <header>
-    <nav class="navbar navbar-expand-lg sticky-top bg-light">
+    <nav
+      id="dresseco-navbar"
+      class="navbar navbar-expand-lg sticky-top bg-light"
+    >
       <div class="container-fluid">
         <a class="navbar-brand" href="https://dresseco.github.io">
           <img
@@ -16,22 +19,46 @@ class Header extends HTMLElement {
             height="64"
           />
         </a>
-        <div class="order-lg-2">
-          <div id="action-buttons">
+        <div class="order-lg-1">
+          <div id="dresseco-action-buttons">
             <button
-              class="btn btn-outline-primary"
+              class="btn btn-outline-primary ms-1 dropdown"
               type="button"
-              onclick="uiSearch()"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
             >
-              buscar
-              <span class="navbar-toggler-icon"></span>
+              <ul class="dropdown-menu">
+                <li>
+                  <h6 class="dropdown-header">Selecciona el teu idioma:</h6>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#"
+                    ><i class="pe-2">🇦🇩</i>Català</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#"
+                    ><i class="pe-2">🇪🇸</i>Español</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#"
+                    ><i class="pe-2">🇬🇧</i>English</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#"
+                    ><i class="pe-2">🇫🇷</i>Français</a
+                  >
+                </li>
+              </ul>
+              <i class="bi bi-globe"></i>
             </button>
-            <button class="btn btn-outline-primary" type="button">
-              carro
-              <span class="navbar-toggler-icon"></span>
+            <button class="btn btn-outline-primary ms-1" type="button">
+              <i class="bi bi-cart-fill pe-2"></i><span>0 productes</span>
             </button>
             <button
-              class="btn btn-outline-primary d-md-none"
+              class="btn btn-outline-primary d-md-none ms-1"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarSupportedContent"
@@ -39,53 +66,49 @@ class Header extends HTMLElement {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              menu
-              <span class="navbar-toggler-icon"></span>
+              <i class="bi bi-list"></i>
             </button>
           </div>
         </div>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <hr />
+          <ul
+            id="dresseco-navbar-pages"
+            class="navbar-nav me-auto mb-2 mb-lg-0"
+          >
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
               <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
+                id="dresseco-navbar-page-home"
+                class="nav-link mx-3"
+                href="https://dresseco.github.io"
+                ><i class="bi bi-house-fill pe-2"></i><span>Inici</span></a
               >
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                </li>
-              </ul>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled">Disabled</a>
+              <a
+                id="dresseco-navbar-page-catalogue"
+                class="nav-link mx-3"
+                href="https://dresseco.github.io/catalogue"
+                ><i class="bi bi-collection-fill pe-2"></i><span>Catàleg</span>
+              </a>
             </li>
           </ul>
-          <form class="d-flex" role="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+          <div class="me-1">
+            <form class="d-flex" role="search">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button
+                class="btn btn-outline-success dresseco-button"
+                type="submit"
+              >
+                <i class="bi bi-search"></i>
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </nav>
